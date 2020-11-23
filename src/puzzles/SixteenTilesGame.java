0e34f4a6649dfdab67b7e90b08aa606351a97ad6
Abhinav Rana <ranaabhinav50@gmail.com>
2020-11-23 13:06:22 +0530

    catalynNumbers and 16tiles solvability

---
+++ src/puzzles/SixteenTilesGame.java
@@ -0,0 +1,12 @@
+package puzzles;
+import java.util.*;
+//https://www.cs.bham.ac.uk/~mdr/teaching/modules04/java2/TilesSolvability.html
+public class SixteenTilesGame {
+	
+	public static void main(String args[]) {
+		
+		
+		
+	}
+	
+}
--- src/puzzles/catalynNumbers.java
+++ src/puzzles/catalynNumbers.java
@@ -1,5 +1,31 @@
 package puzzles;
-
+/* https://cp-algorithms.com/combinatorics/catalan-numbers.html */
+import java.util.*;
 public class catalynNumbers {
-
+	
+	public static void main (String args[]) {
+		
+		/*Catlyn Number series 1 1 2 5 14 42 ...*/
+		/*Recursive Formula */
+		
+		int n = 10;
+		int dp[] = new int[n+1];
+		
+		dp[0] = 1;
+		dp[1] = 1;
+		
+		
+		for(int i = 2 ; i<=n ; i++)
+		{
+			for(int j = 0; j<i ; j++) {
+				dp[i] += dp[j]*dp[i-j-1];
+			}
+		}
+		
+		for(int i: dp) {
+			System.out.print(i+"  ");
+		}
+	}
+	
+	
 }
