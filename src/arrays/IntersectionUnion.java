e1a62fc3ed6cb5c9ca9eddb2f4c6548d9bae0d94
Abhinav Rana <ranaabhinav50@gmail.com>
2020-12-28 11:23:16 +0530

    Create IntersectionUnion.java

---
+++ src/arrays/IntersectionUnion.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class IntersectionUnion {
+	
+	
+	/*Intersection and Union of two sorted arrays can be found easily*/
+	/*Union is printing all the different elements and 
+	 *Intersection is printing only the same elements 
+	 *and since the array is sorted it's no big deal . */
+	
+	// https://www.geeksforgeeks.org/union-and-intersection-of-two-sorted-arrays-2/
+	
+	
+	
+	
+}
