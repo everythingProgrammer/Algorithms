574164d0a84ac7258465a97761a8a9bfe024998f
Abhinav Rana <ranaabhinav50@gmail.com>
2020-12-28 22:57:53 +0530

    Create Jumps.java

---
+++ src/arrays/Jumps.java
@@ -0,0 +1,19 @@
+package arrays;
+import java.util.*;
+public class Jumps {
+	
+	
+	/*Naive Recursive approach
+	 * 
+	 * function(arr, l, h) if(l == h) return 0     int min ... for(i = l+1; i<=h ; i++){ ...
+	 * 
+	 * */
+	
+	
+	
+	
+	public static void main(String args[]) {
+		
+	}
+	
+}
