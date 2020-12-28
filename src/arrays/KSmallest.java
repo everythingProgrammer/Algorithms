bc995e91a31af997c39f726238f0cd63059db89f
Abhinav Rana <ranaabhinav50@gmail.com>
2020-12-28 09:25:07 +0530

    Create KSmallest.java

---
+++ src/arrays/KSmallest.java
@@ -0,0 +1,16 @@
+package arrays;
+
+https://www.geeksforgeeks.org/kth-smallestlargest-element-unsorted-array/
+
+public class KSmallest {
+
+	/*Simple solution will be to do a sort and get the kth element.*/
+	
+	/*Using Min heap and Max heap for getting */
+	
+	/*Using Quick sort for doing this */
+	
+	/*kafi difficult question.*/
+	
+	
+}
