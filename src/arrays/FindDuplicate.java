35ad7f196881ed5af5f466c7f9ac0bf86c03c75b
Abhinav Rana <ranaabhinav50@gmail.com>
2020-12-30 09:28:49 +0530

    Create FindDuplicate.java

---
+++ src/arrays/FindDuplicate.java
@@ -0,0 +1,54 @@
+package arrays;
+
+
+	/*	How can we prove that at least one duplicate number must exist in nums? 
+		Can you solve the problem without modifying the array nums?
+		Can you solve the problem using only constant, O(1) extra space?
+		Can you solve the problem with runtime complexity less than O(n2)?*/
+
+
+/* 1  to modify array elements ( *-1) and when we encounter any -ve array element that index is repeated so ... */
+/* 2  sort the array and check for duplicates .  */
+/* 3  use set  time and space complexity O(n) */
+
+
+/* 4 - Floyd's Tortoise and Hare (Cycle Detection algorithm) 
+ * 
+ *  - similar to finding duplicate node in linkedlist. Same fast pointer and slow pointer 
+ *    but here insted of slow = root and fast = root.next.next (somewhat) 
+ *    we have hare = nums[nums[hare]] tortoise= nums[tortoise] 
+ * 
+ * */
+
+/*Proof is PigeonHole Principal 
+ * https://www.geeksforgeeks.org/discrete-mathematics-the-pigeonhole-principle/ */
+
+
+public class FindDuplicate {
+	
+	
+	public int findDuplicate(int[] nums) {
+        int tortoise = nums[0];
+        int hare = nums[0];
+        
+        tortoise = nums[tortoise];
+        hare = nums[nums[hare]];
+        
+        while(tortoise!=hare){
+            tortoise= nums[tortoise];
+            hare = nums[nums[hare]];
+        }
+        
+        tortoise = nums[0];
+        
+        while(tortoise != hare){
+            tortoise = nums[tortoise];
+            hare = nums[hare];
+        }
+        return hare;
+    }
+	
+	
+	
+	
+}
