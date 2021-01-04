cf404d42f4e4489c5c79cd565564a11ecad3d4f1
Abhinav Rana <ranaabhinav50@gmail.com>
2021-01-04 09:21:42 +0530

    .

---
+++ src/arrays/BuyansSell2.java
@@ -0,0 +1,29 @@
+package arrays;
+
+public class BuyansSell2 {
+	
+	
+	public int maxProfit(int[] prices) {
+		int profit = 0;
+		int i = 0;
+		int valley = 0;
+		int peak = 0;
+		while(i < prices.length-1) {
+			while(  i< prices.length-1 && prices[i] >= prices[i+1]) {
+				i++;
+			}
+			valley = prices[i];
+			
+			while( i< prices.length -1 && prices[i] < prices[i+1]) {
+				i++;
+			}
+			
+			peak = prices[i];
+			
+			profit = profit + (peak -valley);
+		}
+		
+		return 0;
+	}
+	
+}
