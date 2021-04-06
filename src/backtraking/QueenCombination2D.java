f8733de30b195cce343cd1f7ddbfb352b2c4f215
Abhinav Rana <ranaabhinav50@gmail.com>
2021-04-06 21:51:22 +0530

    Update QueenCombination2D.java

--- src/backtraking/QueenCombination2D.java
+++ src/backtraking/QueenCombination2D.java
@@ -4,6 +4,7 @@ public class QueenCombination2D {
 	
 	
 	// simple placing queens ...
+	// this is extra recursive call 
 	public static void QueenCombinationBoxRespect2D(boolean[][] board, int row, int col , int qpsf, int tq , String ans ) {
 		
 		if(qpsf == tq) {
@@ -18,21 +19,46 @@ public class QueenCombination2D {
 		{
 			//place
 			board[row][col] = true;
-			QueenCombinationBoxRespect2D(board, row, col+1, qpsf+1, tq, ans+" b"+qpsf+" r-"+row+" c-"+col);
+			QueenCombinationBoxRespect2D(board, row, col+1, qpsf+1, tq, ans+" {"+row+"-"+col+"}");
 			board[row][col] = false;
 			
 			//don't place
 			QueenCombinationBoxRespect2D(board, row, col+1 , qpsf, tq , ans);
 		}
 		else {
-//			board[row][col] = true;
 			QueenCombinationBoxRespect2D(board, row+1, 0, qpsf, tq, ans);
-//			board[row][col] = false;
 		}
 		
 	}
+
+	// this is manually changing row
+	public static void QueenCombinationBoxRespect2Dcb(boolean[][] board, int row, int col , int qpsf, int tq , String ans) {
+		//positive bc
+		if(qpsf == tq) {
+			System.out.println(ans);
+			return;
+		}
+		
+		// manually variables change
+		if(col == board[0].length) {
+			row++;
+			col = 0;
+		}
+		//negative base case 
+		if(row == board.length) {
+			return ;
+		}
+		//place 
+		board[row][col]  = true;
+		QueenCombinationBoxRespect2Dcb(board, row , col+1, qpsf+1 , tq , ans+" {"+row+"-"+col+"}");
+		board[row][col] = false;
+		QueenCombinationBoxRespect2Dcb(board, row , col+1, qpsf , tq , ans);
+		
+	}
+	
+	
 	public static void main(String args[]) {
-		 QueenCombinationBoxRespect2D(new boolean[2][2],0,0,0,2,""); 
+		 QueenCombinationBoxRespect2Dcb(new boolean[2][2],0,0,0,2,""); 
 	}
 	
 	
