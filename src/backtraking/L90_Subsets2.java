d69db4126de235775350259d56964cdcac50b1ed
Abhinav Rana <ranaabhinav50@gmail.com>
2021-04-09 15:52:55 +0530

    Generate Subsets
    Generate Subset from given array ,  array may contain duplicate elements but your answer should not contain duplicate subsets ,   This unique subset feature has to be extracted from another leetcode question  Question 40 , it is for sorted array while traversing use a if condition and that will do

--- src/backtraking/L90_Subsets2.java
+++ src/backtraking/L90_Subsets2.java
@@ -1,22 +1,25 @@
 package backtraking;
 import java.util.*;
-public class L78_Subsets {
+public class L90_Subsets2 {
 	
-	
-	 public static List<List<Integer>> subsets(int[] nums) {
+	 public static List<List<Integer>> subsetsWithDup(int[] nums) {
 	        
 		 List<List<Integer>>ans = new ArrayList<>();
+		 Arrays.sort(nums);
 		 uniqueElements(nums,0,new ArrayList<Integer>(),ans);
 		 System.out.println(ans);
 		return ans; 
 		 
 	 }
-	 public static void uniqueElements(int nums[] , int curr, ArrayList<Integer>ca ,List<List<Integer>>ans ) {
+	
+	public static void uniqueElements(int nums[] , int curr, ArrayList<Integer>ca ,List<List<Integer>>ans ) {
 		 
 		 ans.add(new ArrayList<>(ca));
 		 
 		 // need a loop here
 		 for(int i = curr ; i<nums.length ; i++) {
+			 if(i> curr && nums[i]== nums[i-1])
+				 continue;
 			 // place
 			 ca.add(nums[i]);
 			 uniqueElements(nums , i+1, ca,ans);
@@ -25,10 +28,10 @@ public class L78_Subsets {
 		 
 	 }
 	
-	
 	public static void main(String args[]) {
 		int nums[] = {1,2,2};
-		subsets(nums);
+		subsetsWithDup(nums);
 	}
+	
 	
 }
